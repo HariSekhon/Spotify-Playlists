0c342a94535a022d72c8ee5eba31fb75e39626eb
Hari Sekhon <harisekhon@gmail.com>
2012-04-15 12:17:14 +0000

    updated paste_playlists.sh

--- spotify/paste_playlists.sh
+++ spotify/paste_playlists.sh
@@ -31,5 +31,21 @@ unsorted_files="
 hangout-rnb
 jay-z
 "
-for x in $sorted_files; do echo "Paste $x:"; cat | sort -f > "$x"; ./find_dups.sh "$x"; echo; echo; done
-for x in $unsorted_files; do echo "Ordered Paste $x:"; cat > "$x"; ./find_dups.sh "$x"; echo; echo; done
+
+paste_nosort(){
+    echo "Ordered Paste $1:"; cat > "$1"; ./find_dups.sh "$1"; echo; echo
+}
+
+paste_sort(){
+    echo "Paste $1:"; cat | sort -f > "$1"; ./find_dups.sh "$1"; echo; echo
+}
+if [ -n "$1" ]; then
+    if [ -n "$2" ]; then
+        paste_nosort "$1"
+    else
+        paste_sort "$1"
+    fi
+else
+    for x in $sorted_files; do paste_sort "$x"; done
+    for x in $unsorted_files; do paste_nosort "$x"; done
+fi
