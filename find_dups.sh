141c2859cc6b09fb21a0d2501872be369fd4c75c
Hari Sekhon <harisekhon@gmail.com>
2012-04-15 19:02:33 +0000

    added find_dups.sh

---
+++ find_dups.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+#
+#   Author: Hari Sekhon
+#   Date: 2012-04-15 19:31:27 +0100 (Sun, 15 Apr 2012)
+#  $LastChangedBy$
+#  $LastChangedDate$
+#  $Revision$
+#  $URL$
+#  $Id$
+#
+#  vim:ts=4:sw=4:et
+
+srcdir=$(dirname $(which $0))
+cd "$srcdir" || exit 1
+
+find_dups(){
+    [ -d "$1" ] && return
+    [[ "$1" =~ .*\.sh ]] && return
+    echo "* Duplicates in $1:"
+    tr 'A-Z' 'a-z' < "$1" | sort | uniq -d
+    echo
+    echo
+}
+
+if [ -n "$1" ]; then
+    for x in $@; do
+        find_dups "$x"
+    done
+else
+    echo ELSE
+    for x in *; do
+        find_dups "$x"
+    done
+fi
