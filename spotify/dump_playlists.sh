aea2d5f4505157d664da61961b1fd29de9074bb6
Hari Sekhon <harisekhon@gmail.com>
2012-05-13 22:27:36 +0000

    updated spotify/dump_playlists.sh

--- spotify/dump_playlists.sh
+++ spotify/dump_playlists.sh
@@ -14,14 +14,33 @@ srcdir=$(dirname $(which $0))
 cd "$srcdir" || exit 1
 total_playlists=0
 total_tracks=0
+
+excluded_file(){
+    [ -d "$1" ] && return 0
+    shopt -s nocasematch
+    [[ "$1" =~ .*\.sh ]] && return 0
+    [[ "$1" = .*\.txt ]] && return 0
+    [[ "$x" =~ .*todo.* ]] && return 0
+    [[ "$x" =~ .*tocheck.* ]] && return 0
+    for y in tmp TODO; do
+        [ "$x" = "$y" ] && return 0
+    done
+    shopt -s nocasematch
+    return 1
+}
+
 dump_playlist(){
-    [ -d "$1" ] && return
-    [[ "$1" =~ .*\.sh ]] && return
-    [ -f "$1" ] || { echo "File not found: $1"; return 1; }
+    local playlist="$1"
+    excluded_file "$playlist" && return 1
+    [ -f "$playlist" ] || { echo "File not found: $playlist"; return 1; }
     let total_playlists+=1
-    let total_tracks+=$(wc -l "$1" | awk '{print $1}')
-    spotify-lookup.pl -v -f "$1" | sort -f > "../$1"
-    echo "Wrote ../$1"
+    let total_tracks+=$(wc -l "$playlist" | awk '{print $1}')
+    if grep -qxFi "$playlist" "$srcdir/sorted_playlists.txt"; then
+        spotify-lookup.pl -v -f "$playlist" | sort -f > "../$playlist"
+    else
+        spotify-lookup.pl -v -f "$playlist" > "../$playlist"
+    fi
+    echo "Wrote ../$playlist"
     echo
     echo
 }
@@ -33,6 +52,7 @@ if [ -n "$1" ]; then
     done
 else
     for x in *; do
+        excluded_file "$x" && continue
         dump_playlist "$x"
     done
 fi
