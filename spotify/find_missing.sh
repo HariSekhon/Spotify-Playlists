e1ff2c707b3c046919ee179e36c1b2d566d158fc
Hari Sekhon <harisekhon@gmail.com>
2012-06-28 19:38:45 +0100

    rewrote find_missing to work with newer spotify-lookup, fixed resolved tracks pwd grepping and added usage and -spotify-uri switch

--- spotify/find_missing.sh
+++ spotify/find_missing.sh
@@ -10,7 +10,7 @@
 #
 #  vim:ts=4:sw=4:et
 
-srcdir=$(dirname $(which $0))
+srcdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$srcdir" || { echo "failed to cd to '$srcdir'"; exit 1; }
 
 current_playlists_default="
@@ -33,39 +33,69 @@ rock
 spotify_lookup=spotify-lookup.pl
 
 find_missing(){
-    echo "* Missing tracks in $1: (not found in "$2")"
+    echo "* Missing tracks in $1: (not found in "$2")" >&2
     tmp=$(
     while read line; do
-        grep -qixF "$line" $2 ||
+        grep -qixF "$line" ${@:2} ||
             echo "$line"
     done < "$1" |
     while read line; do
-        pushd .. >/dev/null
-        track_name="$(spotify-lookup.pl "$line")"
-        grep -qixF "$track_name" $2 2>/dev/null ||
+        if [ $nolookup -eq 0 ]; then
+            # don't need to push we don't be popd'ing
+            cd "$srcdir/.." >&2
+            [ $verbose -gt 0 ] && echo -n "resolving/checking $track => " >&2
+            track_name="$(spotify-lookup.pl <<< "$line")"
+            [ $verbose -gt 0 ] && echo "$track_name" >&2
+            grep -qixF "$track_name" ${@:2} 2>/dev/null ||
+                echo "$line"
+        else
             echo "$line"
+        fi
     done
     )
     # This is because we can't have 2 instance of spotify-lookup.pl running at the same time
     if [ -n "$tmp" ]; then
-        echo "$tmp" | $spotify_lookup
+        if [ $notranslate -eq 1 ]; then
+            echo "$tmp"
+        else
+            echo "$tmp" | $spotify_lookup
+        fi
     fi
     echo
     echo
 }
 
+usage(){
+    cat <<EOF
+${0##*/} [-g] [-nolookup] playlist1 playlist2
+
+-s -spotify-uri         Output Spotify URIs for pasting back in to spotify   
+-n -nolookup            Don't translate at all, don't use spotify-lookup.pl to check artist - song (weakens matching. Only use when spotify-lookup.pl is broken). Also enables -spotify-uri
+-g -grand-playlists     Playlists to check against
+EOF
+    exit 1
+}
+
 current_playlists=""
 grand_playlists=""
 nolookup=0
+notranslate=0
+verbose=0
 while [ $# -gt 0 ]; do
     case $1 in
-        -nolookup)  spotify_lookup=cat
-                    ;;
-               -g)  grand_playlists="$grand_playlists ${2:-}"
-                    shift
-                    ;;
-                *)  current_playlists="$current_playlists $1"
-                    ;;
+           --nolookup)  nolookup=1; notranslate=1
+                        ;;
+     -s|--spotify-uri)  notranslate=1
+                        ;;
+ -g|--grand-playlists)  grand_playlists="$grand_playlists ${2:-}"
+                        shift
+                        ;;
+                   -v)  let verbose+=1
+                        ;;
+                   -*)  usage
+                        ;;
+                    *)  current_playlists="$current_playlists $1"
+                        ;;
     esac
     shift
 done
