852d283d334f688c3571b2a67529fdabc4cd1adf
Hari Sekhon <harisekhon@gmail.com>
2012-07-11 20:22:56 +0100

    updated find_missing_shazam_tracks.sh

--- find_missing_shazam_tracks.sh
+++ find_missing_shazam_tracks.sh
@@ -13,7 +13,7 @@
 set -e
 set -u
 srcdir=$(dirname $(which $0))
-cd "$srcdir" || { echo "Failed to cd to srcdir"; exit 1; }
+cd "$srcdir" || { echo "Failed to cd to srcdir" >&2; exit 1; }
 
 # 1. Used iPhone explorer to copy the whole Shazam dir to Mac
 # 2. grep -R the dir for a known song nme to find the location of the data, was in a sqlite DB
@@ -27,8 +27,10 @@ cd "$srcdir" || { echo "Failed to cd to srcdir"; exit 1; }
 # Turns out the original query was wrong, that's why Artist - Tracks were getting muddled
 #sqlite3 $HOME/Downloads/Shazam-rip-from-phone/Documents/ShazamDataModel.sqlite <<< "select a.zname, b.zname from ZSHARTISTMO a, ZSHTAGRESULTMO b where a.ZTAGRESULT = b.Z_PK;" | sed 's/ [Ff]eat[^-]*|/|/;s/|/.*-.*/' | sort -u |
 #./dump_shazam_tracks.sh
-sed 's/ [Ff]eat[^-]*|/|/;s/|/.*-.*/;s/^The //' < Shazam-dump-$(date +%F).txt |
+shazam_dump=$(ls Shazam-dump-*.txt | tail -n 1)
+echo "using last $shazam_dump" >&2
+sed 's/ [Ff]eat[^-]*|/|/;s/|/.*-.*/;s/^The //' < "$shazam_dump" |
 sort -fu |
 while read track; do
-    grep -qi "$track" dance kiss rock classics-archive chill classical love Shazam-ignore.txt|| echo "$track" | sed 's/\.\*/ /g'
+    grep -qi "$track" dance kiss rock classics-archive chill classical love Shazam-ignore.txt || echo "$track" | sed 's/\.\*/ /g'
 done
