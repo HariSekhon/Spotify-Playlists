64b438a7ce09aa61f37944b848ba1e580a8f1555
Hari Sekhon <harisekhon@gmail.com>
2012-07-21 18:46:27 +0100

    added get.sh

---
+++ spotify/get.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+#
+#   Author: Hari Sekhon
+#   Date: 2012-07-19 12:07:15 +0100 (Thu, 19 Jul 2012)
+#  $LastChangedBy$
+#  $LastChangedDate$
+#  $Revision$
+#  $URL$
+#  $Id$
+#
+#  vim:ts=4:sw=4:et
+
+set -e
+set -u
+srcdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+./paste_playlists.sh $@
+./dump_playlists.sh  $@ -a
