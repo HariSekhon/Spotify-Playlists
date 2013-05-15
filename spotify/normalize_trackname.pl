f2605330ebe01bf67e1a0fb2c4c32149e040f6ef
Hari Sekhon <harisekhon@gmail.com>
2013-05-15 01:39:47 +0100

    filled out normalize_trackname using my lib, added artist ordering

--- spotify/normalize_trackname.pl
+++ spotify/normalize_trackname.pl
@@ -1,4 +1,4 @@
-#!/usr/bin/perl -Tp
+#!/usr/bin/perl -T
 #
 #   Author: Hari Sekhon
 #   Split off from find_missing.sh
@@ -13,11 +13,50 @@
 
 # TODO: turn this in to a propper program with cli args etc
 
-$VERSION="0.1";
+$DESCRIPTION="Normalize Track Names removing edit/version tags";
+
+$VERSION="0.3";
 
 use strict;
 use warnings;
+BEGIN {
+    use File::Basename;
+    use lib dirname(__FILE__) . "/lib";
+}
+use HariSekhonUtils;
 
+my $file;
+my @files;
+
+%options = (
+    "f|file=s"      => [ \$file, "File of track names to normalize. Takes a comma separated list of files. Uses STDIN if no files are specified" ],
+);
+
+get_options();
+
+if($file){
+    my @tmp = split(/\s*,\s*/, $file);
+    push(@files, @tmp);
+}
+
+foreach(@ARGV){
+    push(@files, $_);
+}
+
+( $file and not -f $file ) and die "Error: couldn't find file '$file'\n";
+foreach my $file (@files){
+    if(not -f $file ){
+        print STDERR "File not found: '$file'\n";
+        @files = grep { $_ ne $file } @files;
+    }
+}
+
+vlog_options "files", "[ '" . join("', '", @files) . "' ]";
+
+sub normalize ($) {
+    chomp;
+    /^(?:diff|[+-]{3}|\@\@) / and return;
+    s/^[ +-]//;
 s/^The //i;
 s/\s+(?:-\s+(?:\(|")?|\()
     (?:
@@ -49,6 +88,42 @@ s/\s+(?:-\s+(?:\(|")?|\()
         from|
         theme\s+from|
     )
-    ([\s\)].*)?
-    \n$/\n/xi;
+    (?:[\s\)].*)?
+    $//xi;
 s/( - .+) - Live$/$1/i;
+    # throwing away the first match to make sure I don't hit $1 from above in case there is no featuring
+    s/()\(?feat(?:uring)?\.?\s+(.+)$//i;
+    my $featuring;
+    my @featuring;
+    if(0){
+        $featuring = $2;
+        @featuring = split(/(?:and|\&)/, $featuring);
+        $featuring =~ s/(?:and|\&)//;
+    }
+    my @parts   = split(" - ", $_, 2);
+    my $artists = $parts[0];
+    my $song    = $parts[1];
+    $artists =~ s/ duet with /,/;
+    if(@featuring){
+        $artists .= "," . join(",", @featuring);
+    }
+    #$artists or quit "CRITICAL", "artists string is blank for line '$_'";
+    #$song    or quit "CRITICAL", "song string is blank for line '$_'";
+    unless($artists and $song){
+        vlog2 "skipping line '$_' since \$artists or \$song is blank";
+        print "$_\n";
+        return;
+    }
+    my @artists = split(",", $artists);
+    $artists    = join(",", sort @artists);
+    print "$artists - $parts[1]\n";
+}
+
+if(@files){
+    foreach my $file (@files){
+        open(my $fh, $file) or die "Failed to open file '$file': $!\n";
+        while(<$fh>){ normalize($_) }
+    }
+} else {
+    while(<STDIN>){ normalize($_) }
+}
