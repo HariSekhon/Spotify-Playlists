d5f7fcc361b9999a92956e95680c075eb285ed5f
Hari Sekhon <harisekhon@gmail.com>
2013-05-27 20:33:50 +0100

    updated normalize_tracknames.pl

--- spotify/normalize_tracknames.pl
+++ spotify/normalize_tracknames.pl
@@ -97,7 +97,9 @@ sub normalize ($) {
         (?:[:;\s\)\]].*)?
         $//xi;
     s/( - .+) - Live$/$1/i;
-    s/'\s/ /g;
+    # cuts down on variations to just strip all apostrophes since a lot of compilation song names don't get them right
+    #s/'\s/ /g;
+    s/'/ /g;
     s/\?*$//;
     #s/rmx/Remix/i;
     # added extraction of featuring => artist
