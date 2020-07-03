10bb9cc171ca692a923a45737622d34ce50db6b5
Hari Sekhon <harisekhon@gmail.com>
2020-07-03 18:02:41 +0100

    updated validate_playlist_lengths.sh

--- spotify/validate_playlist_lengths.sh
+++ spotify/validate_playlist_lengths.sh
@@ -1,21 +1,24 @@
 #!/usr/bin/env bash
-#
-#   Author: Hari Sekhon
-#   Date: 2013-05-14 20:08:36 +0100 (Tue, 14 May 2013)
-#  $LastChangedBy$
-#  $LastChangedDate$
-#  $Revision$
-#  $URL$
-#  $Id$
-#
 #  vim:ts=4:sts=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2013-05-14 20:08:36 +0100 (Tue, 14 May 2013)
+#
+#  https://github.com/harisekhon/playlists
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/harisekhon
+#
 
-set -e
-set -u
+set -euo pipefail
+[ -n "${DEBUG:-}" ] && set -x
 srcdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd "$srcdir" || { echo "failed to cd to '$srcdir'"; exit 1; }
 
-status=0 
+cd "$srcdir"
+
 validate_playlist_length(){
     local playlist="$1"
     local tracklist="../$playlist"
@@ -25,19 +28,19 @@ validate_playlist_length(){
     tracklist_wc=$(wc -l "$tracklist" | awk '{print $1}')
     if [ "$playlist_wc" != "$tracklist_wc" ]; then
         echo "Playlist $playlist dump invalid, mismatching number of lines ($playlist $playlist_wc vs $tracklist $tracklist_wc)"
-        status=1
+        exit 1
     else
         echo "Playlist $playlist $playlist_wc => $tracklist $tracklist_wc line counts matched"
     fi
 }
 
-if [ -z "$@" ]; then
-    for x in $(find . -type f | grep -vi -e "\.sh" -e "\.pl" -e "\.txt" -e "\.svn" -e "\.orig" -e "TODO" -e "tocheck"); do
-        validate_playlist_length "$x"
+if [ $# -gt 0 ]; then
+    for playlist in "$@"; do
+        validate_playlist_length "$playlist"
     done
 else
-    for x in $@; do
-        validate_playlist_length "$x"
+    find . -type f | grep -vi -e "\.sh" -e "\.pl" -e "\.txt" -e "\.svn" -e "\.orig" -e "TODO" -e "tocheck" |
+    while read -r filename; do
+        validate_playlist_length "$filename"
     done
 fi
-exit $status
