a0cd3793be3883030e6f51f657d91ec700b8397e
Hari Sekhon <harisekhon@gmail.com>
2020-07-03 18:05:49 +0100

    added validate.sh

---
+++ validate.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2020-07-03 18:05:16 +0100 (Fri, 03 Jul 2020)
+#
+#  https://github.com/harisekhon/playlists
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/harisekhon
+#
+
+set -euo pipefail
+[ -n "${DEBUG:-}" ] && set -x
+srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+"$srcdir/validate_playlist_lengths.sh"
