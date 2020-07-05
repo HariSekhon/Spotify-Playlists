6571897687fe13cf2c342e7a11a5d8e77388d54f
Hari Sekhon <harisekhon@gmail.com>
2020-07-05 11:11:58 +0100

    added convert.sh

---
+++ convert.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2020-07-05 11:00:55 +0100 (Sun, 05 Jul 2020)
+#
+#  https://github.com/harisekhon/spotify-playlists
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/harisekhon
+#
+
+set -euo pipefail
+[ -n "${DEBUG:-}" ] && set -x
+srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# shellcheck disable=SC2034
+usage_description="
+Convert every playlist under spotify/ to human readable format at top level
+using bash-tools/spotify_track_uri_to_name.sh
+
+Git diff should show no differences between this result and the
+spotify_playlist_backup.sh playlists human output at the top level
+"
+
+# used by usage() in lib/utils.sh
+# shellcheck disable=SC2034
+usage_args="[<curl_options>]"
+
+#bash_tools="../bash-tools"
+bash_tools="$srcdir/bash-tools"
+
+# shellcheck disable=SC1090
+. "$bash_tools/lib/utils.sh"
+
+help_usage "$@"
+
+cd "$srcdir"
+
+playlists="$(bash-tools/spotify_playlist_to_filename.sh < playlists.txt)"
+
+while read -r playlist; do
+    timestamp "converting  'spotify/$playlist'  =>  '$playlist'"
+    "$bash_tools/spotify_track_uri_to_name.sh" "$@" < "spotify/$playlist" > "$playlist"
+done <<< "$playlists"
+timestamp "Done"
