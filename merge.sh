ed4995a72f3a88f26077b82d2e97b679305e18ba
Hari Sekhon <harisekhon@gmail.com>
2020-07-05 02:58:39 +0100

    replaced symlink with script wrapper to allow library pathing to work

--- merge.sh
+++ merge.sh
@@ -1,1 +1,20 @@
-bash-tools/scripts/spotify_commit_rename_playlist.sh
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2020-07-05 02:57:34 +0100 (Sun, 05 Jul 2020)
+#
+#  https://github.com/harisekhon/playlists
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/harisekhon
+#
+
+set -euo pipefail
+[ -n "${DEBUG:-}" ] && set -x
+srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+exec "$srcdir/bash-tools/scripts/spotify_commit_rename_playlist.sh" "$@"
