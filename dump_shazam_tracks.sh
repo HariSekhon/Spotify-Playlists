3aaf54fe8377ff1ed226f5f634d5a91866fa723f
Hari Sekhon <harisekhon@gmail.com>
2020-09-25 23:42:43 +0100

    updated dump_shazam_tracks.sh

--- dump_shazam_tracks.sh
+++ dump_shazam_tracks.sh
@@ -1,19 +1,14 @@
 #!/usr/bin/env bash
-#
-#   Author: Hari Sekhon
-#   Date: 2012-06-04 19:04:23 -0700 (Mon, 04 Jun 2012)
-#  $LastChangedBy$
-#  $LastChangedDate$
-#  $Revision$
-#  $URL$
-#  $Id$
-#
 #  vim:ts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2012-06-04 19:04:23 -0700 (Mon, 04 Jun 2012)
+#
 
-set -e
-set -u
-srcdir=$(dirname $(which $0))
-cd "$srcdir" || { echo "Failed to cd to $srcdir"; exit 1; }
+set -eu
+srcdir="$(dirname "${BASE_SOURCE[0]}")"
+
+cd "$srcdir"
 
 # use iPhone Explorer on Mac to copy the /Apps/Shazam folder entirely to Downloads/Shazam-rip-from-phone, free version doesn't allow mounting so that step has to be manual unfortunately
-sqlite3 $HOME/Downloads/Shazam-rip-from-phone/Documents/ShazamDataModel.sqlite <<< "select a.zname, b.zname from ZSHARTISTMO a, ZSHTAGRESULTMO b where a.ZTAGRESULT = b.Z_PK;" > Shazam-dump-$(date +%F).txt
+sqlite3 ~/Downloads/Shazam-rip-from-phone/Documents/ShazamDataModel.sqlite <<< "select a.zname, b.zname from ZSHARTISTMO a, ZSHTAGRESULTMO b where a.ZTAGRESULT = b.Z_PK;" > "Shazam-dump-$(date '+%F').txt"
