5ad48ef687289d3890e03d6aee0c7d1891f2185c
Hari Sekhon <harisekhon@gmail.com>
2021-11-17 13:08:04 +0000

    added load_artist_playlist.sh

---
+++ load_artist_playlist.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2021-11-16 19:22:58 +0000 (Tue, 16 Nov 2021)
+#
+#  https://github.com/HariSekhon/Spotify-Playlists
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/HariSekhon
+#
+
+set -euo pipefail
+[ -n "${DEBUG:-}" ] && set -x
+srcdir="$(dirname "${BASH_SOURCE[0]}")"
+
+# shellcheck disable=SC1090
+. "$srcdir/bash-tools/lib/utils.sh"
+
+# shellcheck disable=SC2034,SC2154
+usage_description="
+Loads all Artist's tracks into a named artist's playlist, from both albums and singles
+
+The artist should have a playlist name matching their artist name
+
+All duplicate and blacklisted tracks are removed afterwards
+"
+
+# used by usage() in lib/utils.sh
+# shellcheck disable=SC2034
+usage_args="<artist>"
+
+help_usage "$@"
+
+min_args 1 "$@"
+
+artist="$1"
+
+export SPOTIFY_PRIVATE=1
+
+spotify_token
+
+"$srcdir/bash-tools/spotify_artist_tracks.sh" "$artist" |
+"$srcdir/bash-tools/spotify_add_to_playlist.sh" "$artist"
+
+echo >&2
+
+"$srcdir/bash-tools/spotify_delete_any_duplicates_in_playlist.sh" "$artist"
+
+echo >&2
+
+# want splitting
+# shellcheck disable=SC2046
+"$srcdir/bash-tools/spotify_delete_from_playlist_if_in_other_playlists.sh" "$artist" $(echo private/Blacklist* | sed 's,.*/,,')
