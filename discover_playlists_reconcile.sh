ad39ab9658e3d9978be195872c639c17ffbf9dc9
Hari Sekhon <harisekhon@gmail.com>
2022-05-10 17:33:48 +0100

    added discover_playlists_reconcile.sh

---
+++ discover_playlists_reconcile.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2022-05-10 16:25:59 +0100 (Tue, 10 May 2022)
+#
+#  https://github.com/HariSekhon/Spotify-Playlists
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/HariSekhon
+#
+
+# Updates the playlist names based on their playlist IDs
+
+set -euo pipefail
+[ -n "${DEBUG:-}" ] && set -x
+srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+bash_tools="$srcdir/bash-tools"
+
+# shellcheck disable=SC1090
+. "$bash_tools/lib/spotify.sh"
+
+cd "$srcdir"
+
+"$bash_tools/decomment.sh" private/discover_playlists.txt |
+while read line; do
+    if ! grep -Fxq "$line" private/playlists_followed.txt; then
+        if grep -Eq $'\t' <<< "$line"; then
+            id="$(awk '{print $1}' <<< "$line")"
+            replacement="$(grep -E "^$id"$'\t' private/playlists_followed.txt || :)"
+            if is_blank "$replacement"; then
+                echo "WARNING: failed to find the playlist with ID '$id'" >&2
+            else
+                sed -i 's|^$id'$'\t''.*|$replacement|' private/discover_playlists.txt
+            fi
+        else
+            echo "WARNING: playlist specified without a playlist ID prefix field, brittle, will likely break when externals rename their playlist:  $line" >&2
+        fi
+    fi
+done
