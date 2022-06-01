8952a8505ab1ed0eea11ad76e91a3b6cd058743e
Hari Sekhon <harisekhon@gmail.com>
2022-06-01 18:29:40 +0100

    updated regenerate_normalized_playlists.sh

--- old/regenerate_normalized_playlists.sh
+++ old/regenerate_normalized_playlists.sh
@@ -1,34 +1,50 @@
 #!/usr/bin/env bash
-#
-#   Author: Hari Sekhon
-#   Date: 2013-05-16 19:32:36 +0100 (Thu, 16 May 2013)
-#  $LastChangedBy$
-#  $LastChangedDate$
-#  $Revision$
-#  $URL$
-#  $Id$
-#
 #  vim:ts=4:sts=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2013-05-16 19:32:36 +0100 (Thu, 16 May 2013)
+#
+#  https://github.com/HariSekhon/Spotify-Playlists
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/HariSekhon
+#
 
-set -e
-set -u
-srcdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd "$srcdir" || { echo "failed to cd to '$srcdir'"; exit 1; }
+set -euo pipefail
+[ -n "${DEBUG:-}" ] && set -x
+srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+cd "$srcdir"
+
 
 #echo "Removing old normalized lists"
 #find . -type f -name '.*' -maxdepth 1 -exec echo rm -v {} \;
 echo "
 Creating new normalized lists
 "
-playlists="$(find . blacklists -type f -maxdepth 1 | sed 's/^\.\///' | grep -vi -e "^\." -e "/\?\." -e "\.sh" -e "\.pl" -e "\.txt" -e "\.svn" -e "\.orig" -e "TODO" -e "tocheck" | sort -t '/' -k2nf)"
+playlists="$(find . blacklists -type f -maxdepth 1 |
+             sed 's/^\.\///' |
+             grep -vi -e '^\.' \
+                      -e '/\?\.' \
+                      -e '\.sh' \
+                      -e '\.pl' \
+                      -e '\.txt' \
+                      -e '\.git' \
+                      -e '\.orig' \
+                      -e 'TODO' \
+                      -e "tocheck" |
+             sort -t '/' -k2nf)"
 
 max_len=0
-while read playlist; do
+while read -r playlist; do
     [ ${#playlist} -gt $max_len ] &&
         max_len=${#playlist}
 done <<< "$playlists"
 
-while read playlist; do
+while read -r playlist; do
     dirname="$(dirname "$playlist")"
     if [ "$dirname" = "." ]; then
         dirname=""
@@ -36,7 +52,7 @@ while read playlist; do
         dirname="$dirname/"
     fi
     basename="$(basename "$playlist")"
-    printf "generating normalized playlist %-${max_len}s => %s\n" "$playlist" "$dirname.$basename"
+    printf "generating normalized playlist %-${max_len}s => %s\\n" "$playlist" "$dirname.$basename"
     spotify/normalize_tracknames.pl "$playlist" > "$dirname.$basename"
 done <<< "$playlists"
 echo "
