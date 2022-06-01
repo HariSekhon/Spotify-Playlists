089a720f11711b7fa5ebba5f6602c9a4ce63d960
Hari Sekhon <harisekhon@gmail.com>
2022-06-01 18:26:48 +0100

    updated find_missing_shazam_tracks.sh

--- find_missing_shazam_tracks.sh
+++ find_missing_shazam_tracks.sh
@@ -1,19 +1,23 @@
 #!/usr/bin/env bash
-#
-#   Author: Hari Sekhon
-#   Date: 2012-06-04 18:11:42 -0700 (Mon, 04 Jun 2012)
-#  $LastChangedBy$
-#  $LastChangedDate$
-#  $Revision$
-#  $URL$
-#  $Id$
-#
 #  vim:ts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2012-06-04 18:11:42 -0700 (Mon, 04 Jun 2012)
+#
+#  https://github.com/HariSekhon/Spotify-Playlists
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/HariSekhon
+#
 
-set -e
-set -u
-srcdir=$(dirname $(which $0))
-cd "$srcdir" || { echo "Failed to cd to srcdir" >&2; exit 1; }
+set -euo pipefail
+[ -n "${DEBUG:-}" ] && set -x
+srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+cd "$srcdir"
 
 # 1. Used iPhone explorer to copy the whole Shazam dir to Mac
 # 2. grep -R the dir for a known song nme to find the location of the data, was in a sqlite DB
@@ -27,10 +31,10 @@ cd "$srcdir" || { echo "Failed to cd to srcdir" >&2; exit 1; }
 # Turns out the original query was wrong, that's why Artist - Tracks were getting muddled
 #sqlite3 $HOME/Downloads/Shazam-rip-from-phone/Documents/ShazamDataModel.sqlite <<< "select a.zname, b.zname from ZSHARTISTMO a, ZSHTAGRESULTMO b where a.ZTAGRESULT = b.Z_PK;" | sed 's/ [Ff]eat[^-]*|/|/;s/|/.*-.*/' | sort -u |
 #./dump_shazam_tracks.sh
-shazam_dump=$(ls Shazam-dump-*.txt | tail -n 1)
+shazam_dump="$(find . -iname 'Shazam-dump-*.txt' | tail -n 1)"
 echo "using last $shazam_dump" >&2
 sed 's/ [Ff]eat[^-]*|/|/;s/|/.*-.*/;s/^The //' < "$shazam_dump" |
 sort -fu |
-while read track; do
-    grep -qi "$track" dance kiss rock classics chill classical love jazz disco Shazam-ignore.txt || echo "$track" | sed 's/\.\*/ /g'
+while read -r track; do
+    grep -qi "$track" dance kiss rock classics chill classical love jazz disco Shazam-ignore.txt || sed 's/\.\*/ /g' <<< "$track"
 done
