74976bd767fd56564dfb548dee5617fce6ead716
Hari Sekhon <harisekhon@gmail.com>
2022-06-01 18:33:56 +0100

    updated find_singles.sh

--- old/find_singles.sh
+++ old/find_singles.sh
@@ -1,17 +1,23 @@
 #!/usr/bin/env bash
-#
-#   Author: Hari Sekhon
-#   Date: 2012-05-21 15:21:50 +0100 (Mon, 21 May 2012)
-#  $LastChangedBy$
-#  $LastChangedDate$
-#  $Revision$
-#  $URL$
-#  $Id$
-#
 #  vim:ts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2012-05-21 15:21:50 +0100 (Mon, 21 May 2012)
+#
+#  https://github.com/HariSekhon/Spotify-Playlists
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/HariSekhon
+#
 
-srcdir="$(dirname "${BASH_SOURCE[0]}")"
-cd "$srcdir" || exit 1
+set -euo pipefail
+[ -n "${DEBUG:-}" ] && set -x
+srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+cd "$srcdir"
 
 find_singles(){
     [ -d "$1" ] && return
