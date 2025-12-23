439ace3f3286cd52a5712e34f1e2e61eaaf5437c
Hari Sekhon <harisekhon@gmail.com>
2025-12-22 21:11:18 -0600

    added reset.sh

---
+++ reset.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+# playlists that have substantial changes that need review, for now just revert and deal with them later
+playlists_to_reset="
+LL Cool J
+Fabulous
+Christmas
+"
+
+while read -r playlist; do
+    [ -z "$playlist" ] && continue
+    echo "Resetting playlist: $playlist"
+    git checkout "$playlist" "spotify/$playlist"
+done <<< "$playlists_to_reset"
