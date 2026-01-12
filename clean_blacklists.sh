2f8913e9c864a425535541396c6380e057e4eb08
Hari Sekhon <harisekhon@gmail.com>
2026-01-12 02:13:15 -0500

    added clean_blacklists.sh

---
+++ clean_blacklists.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2020-07-26 19:58:55 +0100 (Sun, 26 Jul 2020)
+#
+#  https://github.com/HariSekhon/Spotify-Playlists
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn
+#  and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/HariSekhon
+#
+
+set -euo pipefail
+[ -n "${DEBUG:-}" ] && set -x
+srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+bash_tools="$srcdir/bash-tools"
+
+# shellcheck disable=SC1090,SC1091
+. "$bash_tools/lib/spotify.sh"
+
+# shellcheck disable=SC2034,SC2154
+usage_description="
+Deletes tracks from Blacklists that have been accepted into core playlists
+"
+
+# used by usage() in lib/utils.sh
+# shellcheck disable=SC2034
+usage_args=""
+
+help_usage "$@"
+
+no_more_args "$@"
+
+export SPOTIFY_PRIVATE=1
+
+# pre-load token once for deletions and URI=>track name resolving prompt to avoid repeated pop-ups
+spotify_token
+
+# assume we are reasonably up to date since these tracks have a large stock to flow ratio and
+# we can always run a partial backup of just the latest Blacklist playlist manually if we want a fresher list
+#"$srcdir/backup_private.sh" $("$srcdir/bash-tools/spotify_playlists.sh" | grep -E '^Blacklist[[:digit:]]+$')
+
+export CLEANING_BLACKLISTS=1
+
+for playlist in Blacklist{,2,3}; do
+    "$srcdir/delete_tracks_already_in_playlists.sh" "$playlist"
+done
