9d0eade1adfb87160ce80f7310d04c6b4b46c0e6
Hari Sekhon <harisekhon@gmail.com>
2026-01-12 02:04:44 -0500

    updated update_playlist_names.sh

--- update_playlist_names.sh
+++ update_playlist_names.sh
@@ -8,7 +8,8 @@
 #
 #  License: see accompanying Hari Sekhon LICENSE file
 #
-#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback
+#  If you're using my code you're welcome to connect with me on LinkedIn
+#  and optionally send me feedback
 #
 #  https://www.linkedin.com/in/HariSekhon
 #
