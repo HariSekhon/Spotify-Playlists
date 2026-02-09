b1c56ae59844521cc7660c6f216bc0650a866174
Hari Sekhon <harisekhon@gmail.com>
2026-02-09 15:58:42 -0300

    added move_playlists_path_mappings.sh

---
+++ move_playlists_path_mappings.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2026-02-09 15:48:09 -0300 (Mon, 09 Feb 2026)
+#
+#  https///github.com/HariSekhon/Spotify-playlists
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback
+#
+#  https://www.linkedin.com/in/HariSekhon
+#
+
+set -euo pipefail
+[ -n "${DEBUG:-}" ] && set -x
+srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+bash_tools="$srcdir/bash-tools"
+
+if [ -d "$srcdir/../bash-tools" ]; then
+    bash_tools="$srcdir/../bash-tools"
+fi
+
+# shellcheck disable=SC1090,SC1091
+. "$bash_tools/lib/utils.sh"
+
+# shellcheck disable=SC2034,SC2154
+usage_description="
+Moves playlists that match the regex in the local .path_mappings.txt file
+"
+
+# used by usage() in lib/utils.sh
+# shellcheck disable=SC2034
+usage_args=""
+
+help_usage "$@"
+
+no_more_args "$@"
+
+mappings_file="$srcdir/.path_mappings.txt"
+
+mappings="$("$bash_tools/bin/decomment.sh" "$mappings_file")"
+
+while read -r _id playlist; do
+    while read -r dir regex; do
+        if [[ "$playlist" =~ $regex ]]; then
+            "$srcdir/rename.sh" "$playlist" "$dir/"
+            break
+        fi
+    done <<< "$mappings"
+done < "$srcdir/spotify/playlists.txt"
