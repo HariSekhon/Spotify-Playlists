dd224561ee2da805b0e834b87886fe897c86b9b1
Hari Sekhon <harisekhon@gmail.com>
2026-02-11 16:59:56 -0300

    updated find_missing_shazam_tracks.sh

--- scripts/find_missing_shazam_tracks.sh
+++ scripts/find_missing_shazam_tracks.sh
@@ -8,7 +8,8 @@
 #
 #  License: see accompanying Hari Sekhon LICENSE file
 #
-#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+#  If you're using my code you're welcome to connect with me on LinkedIn
+#  and optionally send me feedback to help steer this or other code I publish
 #
 #  https://www.linkedin.com/in/HariSekhon
 #
@@ -17,7 +18,7 @@ set -euo pipefail
 [ -n "${DEBUG:-}" ] && set -x
 srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-cd "$srcdir"
+cd "$srcdir/.."
 
 # 1. Used iPhone explorer to copy the whole Shazam dir to Mac
 # 2. grep -R the dir for a known song nme to find the location of the data, was in a sqlite DB
